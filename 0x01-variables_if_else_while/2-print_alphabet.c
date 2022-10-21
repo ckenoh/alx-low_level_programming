bb79bf0678f434bacae17f7c261a244c1c24ed5b
ckenoh <clintonkenoh@gmail.com>
2022-10-21 03:28:35 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/**
+ * main - entry point
+ * Return: 0
+*/
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'z';
+	while (c <= d)
+	{
+		putchar(c);
+		c++
+	}
+	putchar("\n");
+	return (0);
+}
+
+
+
