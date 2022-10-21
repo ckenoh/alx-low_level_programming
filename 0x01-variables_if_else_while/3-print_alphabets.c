108c3403ea362e67962a76f965d0a74ada0c1261
ckenoh <clintonkenoh@gmail.com>
2022-10-21 10:14:37 -0700

    fix 2 task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,16 +11,16 @@ int main(void)
 
 	c = 'a';
 	d = 'A';
-	while (c <= 'z')
-	{
-		putchar(c);
-		c++;
-	}
-	while (d <= 'z')
-	{
-		putchar(d);
-		d++;
-	}
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(d);
+			d++;
+		}
 	putchar('\n');
 	return (0);
 }
