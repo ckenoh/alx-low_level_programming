244ae05b96addb662ee1a849e94ddc0670bd4cb5
ckenoh <clintonkenoh@gmail.com>
2022-10-22 11:06:09 -0700

    alpha debug task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main - entry point
- *Return: 0
+ * main - entry point
+ * Return: 0
 */
 
 int main(void)
@@ -9,15 +9,15 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= 9; tens++)/* prints tens digit*/
+	for (tens = '0'; tens <= 9; tens++)
 	{
-		for (ones = '0'; ones <= 9; ones++)/* prints ones digit*/
+		for (ones = '0'; ones <= 9; ones++)
 		{
-			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
+			if (!((ones == tens) || (tens > ones)))
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*adds comma and space*/
+				if (!(ones == '9' && tens == '8'))
 				{
 					putchar(',');
 					putchar(' ');
@@ -28,3 +28,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
