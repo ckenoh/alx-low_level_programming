11c5cd9488801af3bcee7a3d35bdb89713ff34be
ckenoh <clintonkenoh@gmail.com>
2022-10-23 10:05:13 -0700

    alpha romeo debug task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - print 10 times the alphabet
-*/
+ */
 
 void print_alphabet_x10(void)
 {
@@ -12,6 +12,6 @@ void print_alphabet_x10(void)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 			_putchar(a);
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
