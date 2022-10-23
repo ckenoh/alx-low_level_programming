6b7242fffab50fd6a85ca1f3281211be22d2ac50
ckenoh <clintonkenoh@gmail.com>
2022-10-23 08:52:50 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lowercase
+ * Return: 0
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n\');
+}
+
