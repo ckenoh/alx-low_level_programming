c891be79fa0781c17a10009f1cace1e6eaaede08
ckenoh <clintonkenoh@gmail.com>
2022-10-23 09:50:59 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet
+*/
+
+void print_alphabet_x10(void)
+{
+	int ten;
+	char a;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+			_putchar('\n');
+		}
+	}
+}
