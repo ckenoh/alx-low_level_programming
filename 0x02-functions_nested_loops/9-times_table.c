698ae5227567357f441a41b792320b3527a579c4
ckenoh <clintonkenoh@gmail.com>
2022-10-23 22:51:21 -0700

    alpha debug task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 /**
  * times_table - prints the 9 times table
  * Return: Always 0
-*/
+ */
 
 void times_table(void)
 {
@@ -22,7 +22,7 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) * '0');
+				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
