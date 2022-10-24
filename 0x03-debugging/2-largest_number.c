51a4858b738b06ab7641a6e1c9c290c9acc3520a
ckenoh <clintonkenoh@gmail.com>
2022-10-24 12:15:06 -0700

    task 2 debug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b) && (c > b))
+	if (a > b)
 	{
 		largest = a;
 	}
-	else if ((b < c) && (a > c))
+	else if (a > c)
 	{
 		largest = a;
 	}
