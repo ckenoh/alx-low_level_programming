be264f58dc21cbd66f55ee62aeacc23ee608a9b8
ckenoh <clintonkenoh@gmail.com>
2022-10-31 00:51:56 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar("%d", a);
+	}
+	return (0);
+}
