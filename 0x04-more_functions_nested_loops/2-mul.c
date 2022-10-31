f1aec957738e89093afee4a9c87f1b85d5995910
ckenoh <clintonkenoh@gmail.com>
2022-10-31 00:32:47 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - mutiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0 (success)
+ */
+
+int mul(int a, int b)
+{
+	int a;
+	int b;
+
+	_putchar ("%d", (a * b);
+			return (0);
+			}
