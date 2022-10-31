d183269ed9f565e83c739f98e1631b035d68171d
ckenoh <clintonkenoh@gmail.com>
2022-10-31 02:46:50 -0700

    cool debug task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * print_most_numbers - prints the numbers from 0 to 9
+ * without 2 and 4
  * @a: first int
  * Return: always 0
  */
@@ -10,10 +11,12 @@ void print_most_numbers(int a)
 {
 	int a = 0;
 
-	if (a <= 9 && a != 2 && a != 4)
+	while (a < 10)
 	{
-		_putchar(a + '0');
+		if (a != 2 && a != 4)
+			_putchar(a + '0');
+
+		a++;
 	}
 	_putchar('\n');
 }
-
