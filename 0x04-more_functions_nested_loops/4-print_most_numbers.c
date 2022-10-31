e0fbf0be9693f69250ed4b4d06219e3346d4b407
ckenoh <clintonkenoh@gmail.com>
2022-10-31 06:36:47 -0700

    final debug task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,11 +3,10 @@
 /**
  * print_most_numbers - prints the numbers from 0 to 9
  * without 2 and 4
- * @a: first int
  * Return: always 0
  */
 
-void print_most_numbers(int a)
+void print_most_numbers(void)
 {
 	int a = 0;
 
