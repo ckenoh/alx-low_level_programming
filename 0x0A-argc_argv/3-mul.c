757d933fe1aea1b72100b7606d20c63f375f6fc7
ckenoh <clintonkenoh@gmail.com>
2022-11-11 02:17:20 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a prgram that multiplies 2 numbers
+ * @argc: the number of arguments
+ * @argv: the names of arguments
+ * Return:Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc > 2)
+	{
+		printf("%d\n", atoi(argv[1] * atoi(argv[2]));
+				return (0);
+				}
+
+				else if (argc < 3)
+				{
+				printf("Error\n");
+				return (1);
+				}
+				return (0);
+				}
+
+
