b20720a2d895096a98f93ae7ce479ee4afd11814
ckenoh <clintonkenoh@gmail.com>
2022-11-14 02:03:22 -0800

    alpha debug task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 
 	if (width < 1 || height < 1)
 		return (NULL);
-	arr = malloc(sizeof(int *) * height);
+	arr = (int*) malloc(sizeof(int) * height);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
